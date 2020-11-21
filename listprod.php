98900369c93cec55372f5871da8a11304eb9beeb
Brian Zhou <brianzhouzc@gmail.com>
2020-11-21 13:07:07 -0800

    Add bootstrap dependencies

--- listprod.php
+++ listprod.php
@@ -1,57 +1,67 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>YOUR NAME Grocery</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<title>YOUR NAME</title>
 </head>
+
 <body>
-<?php
-	include 'include/functions.php';
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+	<div class="container">
 
-	/** Get product name to search for **/
-	if (isset($_GET['productName'])){
-		$name = $_GET['productName'];
-	}
-?>
-	<h1>Search for the products you want to buy:</h1>
+		<?php
+		include 'include/functions.php';
 
-	<form method="get" action="listprod.php">
-	<input type="text" name="productName" size="50" value="<?php echo($name); ?>">
-	<input type="submit" value="Submit"><input id="reset-btn" type="reset" value="Reset"> (Leave blank for all products)
-	</form>
-
-	<script type="text/javascript">
-		document.getElementById('reset-btn').addEventListener("click", function() {
-			window.location.href=removeParam("productName",window.location.href);
-		});
-	</script>
-
-<?php
-	//
-	if (isset($_GET['productName'])){
-		$name = "%" . $name ."%";
-		$con = try_connect();
-		if($con !== false){
-			debug("query: print " . $name);
-			$sql = "SELECT * from product where productName LIKE ?;";
-			$ps = sqlsrv_prepare($con,$sql,array(&$name));
-			$results = sqlsrv_execute($ps);
-			if($results != false){
-				while($product = sqlsrv_fetch_array($ps, SQLSRV_FETCH_ASSOC)){
-					if(!$found_products){
-						$found_products=true;
-					}
-					debug("looping");
-					print_product($product);
-				}
-			}
-			if(!$found_products){
-				echo("no results");
-			}
-			disconnect($con);
+		/** Get product name to search for **/
+		if (isset($_GET['productName'])) {
+			$name = $_GET['productName'];
 		}
-	}
-?>
+		?>
 
+		<h1>Search for the products you want to buy:</h1>
+
+		<form method="get" action="listprod.php">
+			<input type="text" name="productName" size="50" value="<?php echo ($name); ?>">
+			<input type="submit" value="Submit"><input id="reset-btn" type="reset" value="Reset"> (Leave blank for all products)
+		</form>
+
+		<script type="text/javascript">
+			document.getElementById('reset-btn').addEventListener("click", function() {
+				window.location.href = removeParam("productName", window.location.href);
+			});
+		</script>
+
+		<?php
+		if (isset($_GET['productName'])) {
+			$name = "%" . $name . "%";
+			$con = try_connect();
+			if ($con !== false) {
+				debug("query: print " . $name);
+				$sql = "SELECT * from product where productName LIKE ?;";
+				$ps = sqlsrv_prepare($con, $sql, array(&$name));
+				$results = sqlsrv_execute($ps);
+				if ($results != false) {
+					while ($product = sqlsrv_fetch_array($ps, SQLSRV_FETCH_ASSOC)) {
+						if (!$found_products) {
+							$found_products = true;
+						}
+						debug("looping");
+						print_product($product);
+					}
+				}
+				if (!$found_products) {
+					echo ("no results");
+				}
+				disconnect($con);
+			}
+		}
+		?>
+	</div>
 </body>
+
 </html>
