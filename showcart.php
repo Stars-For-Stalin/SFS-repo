cf72bb8f636ef999a7ceaef87eb6ff54d9d52610
Brian Zhou <brianzhouzc@gmail.com>
2020-11-21 22:51:37 -0800

    Move showcart.php to bootstrap

--- showcart.php
+++ showcart.php
@@ -1,50 +1,48 @@
 <?php
 // Get the current list of products
 session_start();
-if(isset($_GET['deleteSession'])){
+if (isset($_GET['deleteSession'])) {
 	unset($_SESSION['productList']);
 	header("Location: showcart.php");
 }
+
+$title = 'Your Shopping Cart';
+include 'include/header.php'
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-<title>Your Shopping Cart</title>
-</head>
 <body>
+	<div class='container'>
+		<?php
+		$productList = null;
+		if (isset($_SESSION['productList']) && !isset($_GET['deleteSession'])) {
+			$productList = $_SESSION['productList'];
+			echo ("<h1>Your Shopping Cart</h1>");
 
-<?php
-include 'include/functions.php';
-$productList = null;
-if (isset($_SESSION['productList']) && !isset($_GET['deleteSession'])){
-	$productList = $_SESSION['productList'];
-	echo("<h1>Your Shopping Cart</h1>");
-	echo("<table><tr><th>Product Id</th><th>Product Name</th><th>Quantity</th>");
-	echo("<th>Price</th><th>Subtotal</th></tr>");
+			echo (make_tableheader(array('Product Id', 'Product Name', 'Quantity', 'Price', 'Subtotal')));
 
-	$total =0;
-	foreach ($productList as $id => $prod) {
-		echo("<tr><td>". $prod['id'] . "</td>");
-		echo("<td>" . $prod['name'] . "</td>");
+			$total = 0;
+			foreach ($productList as $id => $prod) {
+				echo ("<tr><td>" . $prod['id'] . "</td>");
+				echo ("<td>" . $prod['name'] . "</td>");
 
-		echo("<td align=\"center\">". $prod['quantity'] . "</td>");
-		$price = $prod['price'];
+				echo ("<td align=\"center\">" . $prod['quantity'] . "</td>");
+				$price = $prod['price'];
 
-		echo("<td align=\"right\">$" . number_format($price ,2) ."</td>");
-		echo("<td align=\"right\">$" . number_format($prod['quantity']*$price, 2) . "</td></tr>");
-		echo("</tr>");
-		$total = $total +$prod['quantity']*$price;
-	}
-	echo("<tr><td colspan=\"4\" align=\"right\"><b>Order Total</b></td><td align=\"right\">$" . number_format($total,2) ."</td></tr>");
-	echo("</table>");
+				echo ("<td align=\"right\">$" . number_format($price, 2) . "</td>");
+				echo ("<td align=\"right\">$" . number_format($prod['quantity'] * $price, 2) . "</td></tr>");
+				echo ("</tr>");
+				$total = $total + $prod['quantity'] * $price;
+			}
+			echo ("<tr><td colspan=\"4\" align=\"right\"><b>Order Total</b></td><td align=\"right\">$" . number_format($total, 2) . "</td></tr>");
+			echo ("</table>");
 
-	echo("<h2><a href=\"checkout.php\">Check Out</a></h2>");
-} else{
-	echo("<H1>Your shopping cart is empty!</H1>");
-}
-?>
-<h2><a href="listprod.php">Continue Shopping</a></h2>
+			echo ("<h2><a href=\"checkout.php\">Check Out</a></h2>");
+		} else {
+			echo ("<H1>Your shopping cart is empty!</H1>");
+		}
+		?>
+		<h2><a href="listprod.php">Continue Shopping</a></h2>
+	</div>
 </body>
-</html> 
 
+</html>
