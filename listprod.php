22d80dabf0202288e8980e94087b653768f7cea5
Brian Zhou <brianzhouzc@gmail.com>
2020-11-22 00:46:56 -0800

    Move listprod.php to bootstrap

--- listprod.php
+++ listprod.php
@@ -1,59 +1,69 @@
 <!DOCTYPE html>
 <html>
-<?php 
-    $title = "Stars for Stalin - Buy Now Before They're Gone"; 
-    include 'include/header.php';
-		//include 'include/functions.php';
+<?php
+$title = "Stars for Stalin - Buy Now Before They're Gone";
+include 'include/header.php';
+//include 'include/functions.php';
 
-		/** Get product name to search for **/
-		if (isset($_GET['productName'])){
-			$name = $_GET['productName'];
-		}
+/** Get product name to search for **/
+if (isset($_GET['productName'])) {
+	$name = $_GET['productName'];
+}
 ?>
+
 <body>
-    <div class="container">
+	<div class="container">
 		<h1>Search for stars:</h1>
 
-		<form method="get" action="listprod.php">
-		<input type="text" name="productName" size="50" value="<?php echo($name); ?>">
-		<input type="submit" value="Submit"><input id="reset-btn" type="reset" value="Reset"> (Leave blank for all products)
+		<form class='form-inline' method="get" action="listprod.php">
+			<div class='col-8'>
+				<input style='width:100%' class='form-control' type="text" name="productName" placeholder='Leave blank for all products' value="<?php echo ($name); ?>">
+			</div>
+			<div class='col-2'>
+				<input class='form-control btn btn-primary' type="submit" value="Submit">
+				<input class='form-control btn btn-secondary' id="reset-btn" type="reset" value="Reset">
+
+			</div>
 		</form>
+	</div>
 
-		<script type="text/javascript">
-			document.getElementById('reset-btn').addEventListener("click", function() {
-				window.location.href=removeParam("productName",window.location.href);
-			});
-		</script>
+	<script type="text/javascript">
+		document.getElementById('reset-btn').addEventListener("click", function() {
+			window.location.href = removeParam("productName", window.location.href);
+		});
+	</script>
 
-<?php
-		$name = "%" . $name ."%";
+	<div class="mt-3 container">
+		<?php
+		$name = "%" . $name . "%";
 		$con = try_connect();
-		if($con !== false){
+		if ($con !== false) {
 			debug_to_console("query: print " . $name);
 			$sql = "SELECT * from product where productName LIKE ?;";
-			$ps = sqlsrv_prepare($con,$sql,array(&$name));
+			$ps = sqlsrv_prepare($con, $sql, array(&$name));
 			$exec_success = sqlsrv_execute($ps);
-            $arr = array();
-			if($exec_success != false){
-				while($product = sqlsrv_fetch_array($ps, SQLSRV_FETCH_ASSOC)){
-					if(!$found_products){
-						$found_products=true;
-                        echo(make_tableheader(array("","Product Name","Price")));
+			$arr = array();
+			if ($exec_success != false) {
+				while ($product = sqlsrv_fetch_array($ps, SQLSRV_FETCH_ASSOC)) {
+					if (!$found_products) {
+						$found_products = true;
+						echo (make_tableheader(array("", "Product Name", "Price")));
 					}
 					debug_to_console("looping");
 					print_product($product);
 				}
 			} else {
-                debug_to_console("SQL query failed.");
-            }
-			if(!$found_products){
-				echo("no results");
+				debug_to_console("SQL query failed.");
+			}
+			if (!$found_products) {
+				echo ("no results");
 			} else {
-                echo("</table>");
-            }
+				echo ("</table>");
+			}
 			disconnect($con);
 		}
-?>
+		?>
 	</div>
 </body>
+
 </html>
