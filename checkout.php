2944f2aaa2c34956e80e51f183dccd2cf69242b0
Brian Zhou <brianzhouzc@gmail.com>
2020-11-22 12:22:07 -0800

    Move checkout login page to bootstrap & added password box

--- checkout.php
+++ checkout.php
@@ -1,17 +1,48 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Grocery CheckOut Line</title>
-</head>
+<?php
+$title = 'Grocery CheckOut Line';
+include 'include/header.php';
+?>
+
+
 <body>
 
-<h1>Enter your customer id to complete the transaction:</h1>
+    <div class='container'>
+        <div class="row">
+            <aside class="col-sm-4"></aside>
 
-<form method="get" action="order.php">
-<input type="text" name="customerId" size="50">
-<input type="submit" value="Submit"><input type="reset" value="Reset">
-</form>
+            <aside class="col-sm-4">
+                <div class="alert alert-info" role="alert">
+                    Please login to complete the transaction:
+                </div>
 
+                <div class="card">
+                    <article class="card-body">
+                        <button class="float-right btn btn-outline-primary" onclick='document.getElementById("login").reset();'>Reset</button>
+                        <h4 class="card-title mb-4 mt-1">Sign in</h4>
+
+                        <form id='login' data-bitwarden-watching="1" action="order.php" method="get">
+
+                            <div class="form-group">
+                                <label>Your customer ID</label>
+                                <input name="customerId" class="form-control" placeholder="123456789" type="number">
+                            </div>
+                            <div class="form-group">
+                                <label>Your password</label>
+                                <input name="password" class="form-control" placeholder="******" type="password">
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-block"> Login </button>
+                            </div>
+                        </form>
+                    </article>
+                </div>
+            </aside>
+
+            <aside class="col-sm-4"></aside>
+
+        </div>
+    </div>
 </body>
-</html>
 
+</html>
