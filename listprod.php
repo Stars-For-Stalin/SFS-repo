ce4681b041b9ff7b16eeee2bfd8b9ffdddeb1652
Josh Cooper <cooper.s.josh@outlook.com>
2020-11-21 19:07:02 -0800

    Integrates header.php with listprod.php

--- listprod.php
+++ listprod.php
@@ -1,24 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	<title>Stars for Stalin - Buy Now Before They're Gone</title>
-</head>
-<body>
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-	<div class="container">
-<?php
-		include 'include/functions.php';
+<?php 
+    $title = "Stars for Stalin - Buy Now Before They're Gone"; 
+    include 'include/header.php';
+		//include 'include/functions.php';
 
 		/** Get product name to search for **/
 		if (isset($_GET['productName'])){
 			$name = $_GET['productName'];
 		}
 ?>
+<body>
+    <div class="container">
 		<h1>Search for stars:</h1>
 
 		<form method="get" action="listprod.php">
