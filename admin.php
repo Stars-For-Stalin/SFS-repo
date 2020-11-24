40316c554099f9329d499b0d7350e56089a7f1a8
Josh Cooper <cooper.s.josh@outlook.com>
2020-11-23 17:37:44 -0800

    Adds lab 8 files (non-conflicting)

---
+++ admin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Administrator Page</title>
+</head>
+<body>
+
+<?php 
+// TODO: Include files auth.php and include/db_credentials.php
+?>
+
+<?php
+// TODO: Write SQL query that prints out total order amount by day
+?>
+</body>
+</html>
---
+++ product.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ray's Grocery - Product Information</title>
+</head>
+<body>
+
+<?php 
+    include 'header.php';
+    include 'include/db_credentials.php';
+?>
+
+<?php
+// Get product name to search for
+// TODO: Retrieve and display info for the product
+// $id = $_GET['id'];
+
+$sql = "";
+
+// TODO: If there is a productImageURL, display using IMG tag
+
+// TODO: Retrieve any image stored directly in database. Note: Call displayImage.php with product id as parameter.
+
+// TODO: Add links to Add to Cart and Continue Shopping
+?>
+</body>
+</html>
