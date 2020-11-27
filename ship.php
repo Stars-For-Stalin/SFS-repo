e4bf936ef5dc07e3853241454c4a7e9863a27844
Josh Cooper <cooper.s.josh@outlook.com>
2020-11-27 15:11:24 -0800

    Adds formatting to ship.php

--- ship.php
+++ ship.php
@@ -1,27 +1,25 @@
 <?php
-include 'include/header.php';
+	include 'include/header.php';
 
-$id = $_GET['id'];
+	$id = $_GET['id'];
 ?>
 
 <body>
-
-	<div class='container'>
-		<?php
-
+<div class='container'>
+	<?php
 		$con = try_connect();
 
 		if ($con == false)
 			die('Error connection to DB');
 
-		// TODO: Get order id      
+		// TODO: Get order id
 		// TODO: Check if valid order id
 		if (is_numeric($id)) {
 			$sql1 = 'SELECT * FROM ordersummary WHERE orderId = ?';
 			$ps1 = sqlsrv_prepare($con, $sql1, array($id));
 			$result1 = sqlsrv_execute($ps1);
 			if (!is_null(sqlsrv_fetch_array($ps1, SQLSRV_FETCH_ASSOC))) {
-				// TODO: Start a transaction 
+				// TODO: Start a transaction
 				sqlsrv_begin_transaction($con);
 
 				// TODO: Retrieve all items in order with given id
@@ -44,29 +42,36 @@ $id = $_GET['id'];
 				$sql4 = 'SELECT * FROM productinventory WHERE productId = ? AND warehouseId = 1';
 				$sql5 = 'UPDATE productinventory SET quantity = quantity - ? WHERE productId = ?';
 				$insufficient_qty = false;
+
+				echo("<h2>Order Id #$id</h2>");
+				echo(make_tableheader(array("<h4>Product Id</h4>","<h4>Ordered Quantity</h4>","<h4>Inventory</h4>","<h4>Inventory After Shipment</h4>")));
 				foreach ($order_products as $prodId => $quantity) {
 					$ps4 = sqlsrv_prepare($con, $sql4, array($prodId));
 					$result4 = sqlsrv_execute($ps4);
 					$row4 = sqlsrv_fetch_array($ps4, SQLSRV_FETCH_ASSOC);
-
-					if ($row4['quantity'] >= $quantity) {
-						echo ('Ordered Product: ' . $prodId . ' Qty: ' . $quantity);
-						echo (' Pre inv: ' . $row4['quantity'] . ' Post inv: ' . ($row4['quantity']  -  $quantity));
-						echo ('<br>');
+					$inv = $row4['quantity'];
+					if ($inv >= $quantity) {
+						echo(make_row(
+							array(
+								make_cell($prodId),
+								make_cell($quantity),
+								make_cell($inv),
+								make_cell($inv - $quantity)
+							)));
 						$ps5 = sqlsrv_prepare($con, $sql5, array($quantity, $prodId));
 						$result5 = sqlsrv_execute($ps5);
 					} else {
 						$insufficient_qty = true;
 					}
 				}
-
+				echo("</table>");
 				// TODO: Make sure to commit or rollback active transaction
 				if (!$insufficient_qty) {
 					sqlsrv_commit( $con );
 					echo ('Success');
 				} else {
 					sqlsrv_rollback( $con );
-					echo ('Insufficient Quantity');
+					echo ("<h2>Shipment not done. Insufficient inventory for product id: $prodId</h2>");
 				}
 			} else {
 				echo ("Order doesn't exsist");
@@ -76,10 +81,9 @@ $id = $_GET['id'];
 		}
 
 		disconnect($con);
-		?>
+	?>
 
-		<h2><a href="shop.html">Back to Main Page</a></h2>
-	</div>
+    <h2><a href="shop.html">Back to Main Page</a></h2>
+</div>
 </body>
-
 </html>
