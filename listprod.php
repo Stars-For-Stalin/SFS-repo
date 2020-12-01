680d656ad43d961554f387827384d0327673ebf6
Josh Cooper <cooper.s.josh@outlook.com>
2020-11-30 20:37:22 -0800

    Implements category searching

--- listprod.php
+++ listprod.php
@@ -1,18 +1,40 @@
 <?php
-$title = "Get Yours Before They're Gone: Stars For Stalin";
-include 'include/header.php';
+	$title = "Get Yours Before They're Gone: Stars For Stalin";
+	include 'include/header.php';
 
-/** Get product name to search for **/
-if (isset($_GET['productName'])) {
-    $name = $_GET['productName'];
-}
+	/** Get product name to search for **/
+	if (isset($_GET['productName'])) {
+		$name = $_GET['productName'];
+	}
+	if (isset($_GET['categoryName'])) {
+		$category = $_GET['categoryName'];
+	}
+	$con = try_connect();
 ?>
 
 <body>
     <div class="container">
         <h1>Search for stars:</h1>
-
         <form class='form-inline' method="get" action="listprod.php">
+            <select size="1" name="categoryName">
+                <?php
+                    if ($con !== false) {
+                        $sql = "select * from category;";
+						$results = sqlsrv_query($con, $sql, array());
+						$options = "<option>All</option>";
+						while ($row = sqlsrv_fetch_array($results, SQLSRV_FETCH_ASSOC)) {
+						    $cat = $row['categoryName'];
+						    if($cat == $category){
+						        $categoryId = (int)$row['categoryId'];
+								$options = "<option>$category</option>$options";
+                            } else {
+						        $options = "$options<option>$cat</option>";
+                            }
+						}
+						echo($options);
+                    }
+                ?>
+            </select>
             <div class='col-8'>
                 <input style='width:100%' class='form-control' type="text" name="productName" placeholder='Leave blank for all products' value="<?php echo ($name); ?>">
             </div>
@@ -26,41 +48,45 @@ if (isset($_GET['productName'])) {
 
     <script type="text/javascript">
         document.getElementById('reset-btn').addEventListener("click", function() {
-            window.location.href = removeParam("productName", window.location.href);
+            window.location.href = removeParam("categoryName", removeParam("productName", window.location.href));
         });
     </script>
 
     <div class="mt-3 container">
         <?php
-        $name = "%" . $name . "%";
-        $con = try_connect();
-        if ($con !== false) {
-            debug_to_console("query: print " . $name);
-            $sql = "SELECT * from product where productName LIKE ?;";
-            $ps = sqlsrv_prepare($con, $sql, array(&$name));
-            if (sqlsrv_execute($ps)) {
-                while ($product = sqlsrv_fetch_array($ps, SQLSRV_FETCH_ASSOC)) {
-                    if (!$found_products) {
-                        $found_products = true;
-                        echo (make_tableheader(array(
-                            "",
-                            "Product Name",
-                            "Price"
-                        )));
-                    }
-                    debug_to_console("looping");
-                    print_product($product);
+			$name = "%" . $name . "%";
+			if ($con !== false) {
+                debug_to_console("query: print " . $name);
+                if(is_null($categoryId)) {
+					$sql = "SELECT * from product where productName LIKE ?;";
+					$ps = sqlsrv_prepare($con, $sql, array(&$name));
+				} else {
+					$sql = "SELECT * from product where productName LIKE ? and categoryId = ?;";
+					$ps = sqlsrv_prepare($con, $sql, array(&$name,&$categoryId));
                 }
-            } else {
-                debug_to_console("SQL query failed.");
+                if (sqlsrv_execute($ps)) {
+                    while ($product = sqlsrv_fetch_array($ps, SQLSRV_FETCH_ASSOC)) {
+                        if (!$found_products) {
+                            $found_products = true;
+                            echo (make_tableheader(array(
+                                "",
+                                "Product Name",
+                                "Price"
+                            )));
+                        }
+                        debug_to_console("looping");
+                        print_product($product);
+                    }
+                } else {
+                    debug_to_console("SQL query failed.");
+                }
+                if (!$found_products) {
+                    echo ("no results");
+                } else {
+                    echo ("</table>");
+                }
+                disconnect($con);
             }
-            if (!$found_products) {
-                echo ("no results");
-            } else {
-                echo ("</table>");
-            }
-            disconnect($con);
-        }
         ?>
     </div>
 </body>
