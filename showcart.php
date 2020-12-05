369952ada4069d403426facd1c644a91dbce7f2c
Josh Cooper <cooper.s.josh@outlook.com>
2020-12-04 17:52:28 -0800

    Adds remove button - non-functional

--- showcart.php
+++ showcart.php
@@ -18,36 +18,47 @@
 				if (isset($_SESSION['productList'])) {
 					$productList = $_SESSION['productList'];
 					echo ("<h1>Your Shopping Cart</h1>");
-
-					echo (make_tableheader(array('Product Id', 'Product Name', 'Quantity', 'Price', 'Subtotal')));
+					echo (make_tableheader(array('Product Id', 'Product Name', 'Quantity', 'Price', 'Subtotal','')));
 
 					$total = 0;
 					foreach ($productList as $id => $prod) {
-						echo ("<tr><td>" . $prod['id'] . "</td>");
-						echo ("<td>" . $prod['name'] . "</td>");
+					    $name = $prod['name'];
+					    $quantity = $prod['quantity'];
+					    $price = $prod['price'];
+					    $subtotal = $quantity * $price;
+					    $subtotal_str = number_format($subtotal,2);
+					    $remove_btn = "<input class='form-control btn btn-md btn-danger' id='reset-btn' type='reset' value='remove'>";
 
-						echo (make_cell(
-							'<input class="form-control" type="number" min="0" value="' . $prod['quantity'] . '" name="prod_' . $prod['id'] . '">',
-							'td',
-							array('style' => 'width:10%')
-						));
-						$price = $prod['price'];
-						$subtotalnum = $price * $prod['quantity'];
-						$subtotal = number_format($subtotalnum,2);
+					    $cells = array();
+						array_push($cells,make_cell($id));
+						array_push($cells,make_cell($name));
+						$quantity_row =
+							"<div class='row pl-3'>" .
+                                "<input class='form-control col-4' type='number' min='0' value='$quantity' name='prod_$id'>".
+							    "<div class='col-8 text-right'>$remove_btn</div>".
+							"</div>";
+						$attr = array("style" => "width:25%");
+						array_push($cells,make_cell($quantity_row,"td",$attr));
+						$attr = array("class" => "text-right");
+						array_push($cells,make_cell("$$price","td",$attr));
+						array_push($cells,make_cell("$$subtotal_str","td",$attr));
 
-						echo ("<td class='text-right'>$$price</td>");
-						echo ("<td class='text-right'>$$subtotal</td>");
-						echo ("</tr>");
-						$total = $total + $subtotalnum;
+						echo(make_row($cells));
+						$total = $total + $subtotal;
 					}
 					$total = number_format($total,2);
-					echo ("<tr><td colspan=\"4\" class='text-right'><b>Order Total</b></td><td class='text-right'>$$total</td></tr>");
+					echo ("<tr><td colspan='4' class='text-right'><b>Order Total</b></td><td class='text-right'>$$total</td></tr>");
 					echo ("</table>");
 				} else {
 					echo ("<H1>Your shopping cart is empty!</H1>");
 				}
 			?>
 
+            <script type="text/javascript">
+                //document.getElementById('reset-btn').addEventListener("click", function() {
+                //    window.location.href = removeParam("categoryName", removeParam("productName", window.location.href));
+                //});
+            </script>
 			<div class="row">
 				<div class="col-lg-8 col-sm-6">
 					<a class="btn btn-secondary btn-md" href="listprod.php">Continue Shopping</a>
