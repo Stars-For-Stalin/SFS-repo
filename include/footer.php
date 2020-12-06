06fa417af48eca439bbc5dac3addf4d6c0dda18e
Josh Cooper <cooper.s.josh@outlook.com>
2020-12-05 21:27:45 -0800

    Fixes footer placement

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,4 @@
-<footer>
+<footer class="fixed-bottom">
     <nav class="footer navbar-light bg-light">
         <div class="navbar-nav mr-auto">
             <h6 class="text-left"><a class="nav-item nav-link" href="admin.php">Admin Panel</a></h6>
