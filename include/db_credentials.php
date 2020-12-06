ee6348112e9a1b3f56fc35a8a1b6c9175de83460
Josh Cooper <cooper.s.josh@outlook.com>
2020-12-05 18:17:37 -0800

    Modifies credentials

--- include/db_credentials.php
+++ include/db_credentials.php
@@ -1,14 +1,7 @@
 <?php
-	$username = "todo";
-	$password = "todo";
-	$database = "db_" . $username;
-	$server = "sql04.ok.ubc.ca";
-	$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
-?>
-<?php
 	$username = "SA";
 	$password = "YourStrong@Passw0rd";
 	$database = "tempdb";
-	$server = "db";
+	$server = ".";
 	$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
 ?>
