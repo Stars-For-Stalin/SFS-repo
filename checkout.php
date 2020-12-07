e1b20ca0fab7751187187d998bc2e26bf55c990b
Brian Z <me@brianzhou.co>
2020-12-06 23:00:59 -0800

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -2,4 +2,9 @@
 	session_start();
 	$title = 'Grocery CheckOut Line: Stars For Stalin';
 
-	include 'include/auth.php';
+	if (isset($_SESSION['authenticatedUser'])) {
+		header('Location: order.php');
+	} else {
+		$_SESSION['loginMessage'] = 'Please login to complete checkout!';
+		header('Location: account/login.php?redirect=checkout.php');
+	}
