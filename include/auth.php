0a45834091df12818258b7a3feef6f92078eec85
Josh Cooper <cooper.s.josh@outlook.com>
2020-12-06 22:56:14 -0800

    Fixes auth.php

--- include/auth.php
+++ include/auth.php
@@ -6,10 +6,10 @@
 	if (!$authenticated) {
 		$loginMessage = "You have not been authorized to access the URL " . $_SERVER['REQUEST_URI'];
 		$_SESSION['loginMessage'] = $loginMessage;
-		header('Location: login.php?redirect=' . $_SERVER['REQUEST_URI']);
+		header('Location: /account/login.php?redirect=' . $_SERVER['REQUEST_URI']);
 	} elseif (isset($auth_admin_only) && $auth_user != "admin") {
 		$loginMessage = "You must be logged in as admin to access the page " . $_SERVER['REQUEST_URI'];
 		$_SESSION['loginMessage'] = $loginMessage;
-		header('Location: login.php?redirect=' . $_SERVER['REQUEST_URI']);
+		header('Location: /account/login.php?redirect=' . $_SERVER['REQUEST_URI']);
 	}
 ?>
